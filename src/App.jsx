c6de84b68bc2e6f82ff5cf17bd57a5e589402dc6
thanhcr7goat <thanh04032004@gmail.com>
2025-08-02 22:19:38 +0700

    Add theme

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
 import HomeIcon from '@mui/icons-material/Home'
 import { pink } from '@mui/material/colors'
+import { Typography } from '@mui/material'
 
 function App() {
 
   return (
     <>
       <div>Hello World</div>
+
+      <Typography variant='body2' color="text.secondary">test</Typography>
+
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
